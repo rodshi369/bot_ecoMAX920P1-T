0a726099017524d452f3f27692db8f62ce55b9ca
rodshi369 <130316521+rodshi369@users.noreply.github.com>
2023-09-24 03:14:21 +0300

    Update set.py

--- set.py
+++ set.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 # Токен телеграмм бота
-TOKEN = '5964513260:AAGfL799eqdDgC4Br-1NaxWAK2S4ldt1jD8' #Бот t.me/PR18DC_BOT
+TOKEN = '' 
 
 IP = ''
-PORT = 8008
+PORT = 
 
 DEVICE = "com3"
 BAUDRATE = 115200
